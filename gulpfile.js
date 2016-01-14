6b2a423d5341d850a6e456e6063da4ed818a4a40
Paul Murray <plmrry@gmail.com>
2016-01-14 17:02:26 -0600

    add package.json and gulpfile.js for starting mongo

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var spawn = require('child_process').spawn;
+
+gulp.task('start-mongo', () => {
+  const mongod = spawn('mongod', ['--dbpath','./data'])
+  mongod.stdout.pipe(process.stdout)
+})
+
+// // http://stackoverflow.com/a/28912360/502331
+// function runCommand(command) {
+//   return function (cb) {
+//     exec(command, function (err, stdout, stderr) {
+//       console.log(stdout);
+//       console.log(stderr);
+//       cb(err);
+//     });
+//   }
+// }
+
+// Running mongo
+// http://stackoverflow.com/a/28048696/46810
+// gulp.task('start-mongo', runCommand('mongod --dbpath ./data/'));
+// gulp.task('start-mongo', () => {
+//   exec('mongod --dbpath ./data', function (err, stdout, stderr) {
+//     console.log('im a baby')
+//     console.log(stdout);
+//     console.log(stderr);
+//     cb(err);
+//   });
+// });
